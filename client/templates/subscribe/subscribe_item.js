c1c6b7ef4f03465782f104b621e85d729778f772
danielmoralesp <daniel_moralesp@hotmail.com>
2016-09-28 17:31:14 -0500

    formulario de susbscripcion terminado

--- client/templates/subscribe/subscribe_item.js
+++ client/templates/subscribe/subscribe_item.js
@@ -1,12 +1,35 @@
-Shower({
-    name:"subscribeForm",
-    method:"subscribeInsert",
-    fields:{
-        email:{
-            required:true,
-            format:"email",
-            message:"Ingrese un email valido"
-        }
-    }
+Template.subscribeItem.onCreated(function() {
+  Session.set('subscribeItemErrors', {});
 });
 
+Template.subscribeItem.helpers({
+  errorMessage: function(field) {
+    return Session.get('subscribeItemErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('subscribeItemErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.subscribeItem.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    var $email = $(e.target).find('[name=email]');
+    var subscriptor = {
+      email: $email.val()
+    };
+    var errors = {};
+    if (! subscriptor.email) {
+      errors.email = "Porfavor escriba un email";
+      return Session.set('subscribeItemErrors', errors);
+    }
+    Meteor.call('subscribeInsert',subscriptor, function(error, subscriptors){
+      // display the error to the user and abort
+      if (error)
+        return throwError(error.reason);
+      
+      Router.go('home');
+      document.forms['subscribe'].reset();
+    });
+  }
+});
