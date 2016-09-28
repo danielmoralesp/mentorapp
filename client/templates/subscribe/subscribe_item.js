17a8f119bf722fda68f9eebcbea1a58a94d052cc
danielmoralesp <daniel_moralesp@hotmail.com>
2016-09-28 16:15:37 -0500

    validacion en el lado del cliente del email de forma correcta, falta la validacion en el server

--- client/templates/subscribe/subscribe_item.js
+++ client/templates/subscribe/subscribe_item.js
@@ -1,33 +1,12 @@
-Template.subscribeItem.onCreated(function() {
-  Session.set('subscribeItemErrors', {});
-});
-
-Template.subscribeItem.helpers({
-  errorMessage: function(field) {
-    return Session.get('subscribeItemErrors')[field];
-  },
-  errorClass: function (field) {
-    return !!Session.get('subscribeItemErrors')[field] ? 'has-error' : '';
-  }
-});
-
-Template.subscribeItem.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    var $email = $(e.target).find('[name=email]');
-    var subscriptor = {
-      email: $email.val()
-    };
-    var errors = {};
-    if (! subscriptor.email) {
-      errors.email = "Porfavor escriba un email";
-      return Session.set('subscribeItemErrors', errors);
+Shower({
+    name:"subscribeForm",
+    method:"subscribeInsert",
+    fields:{
+        email:{
+            required:true,
+            format:"email",
+            message:"Ingrese un email valido"
+        }
     }
-    Meteor.call('subscribeInsert',subscriptor, function(error, subscriptors){
-      // display the error to the user and abort
-      if (error)
-        return throwError(error.reason);
-      Router.go('home');
-    });
-  }
 });
+
